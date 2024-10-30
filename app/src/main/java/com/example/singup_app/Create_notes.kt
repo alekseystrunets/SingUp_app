ef75608f99beb21ff63d51529c80c2e61a272491
ALeksey Strunets <a.strunetss@gmail.com>
2024-10-30 21:47:33 +0300

    Initial commit

---
+++ app/src/main/java/com/example/singup_app/Create_notes.kt
@@ -0,0 +1,43 @@
+package com.example.singup_app
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
+
+class Create_notes : AppCompatActivity() {
+    private lateinit var headerEditText: AppCompatEditText
+    private lateinit var dateEditText: AppCompatEditText
+    private lateinit var messageEditText: AppCompatEditText
+    private lateinit var createButton: AppCompatButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_notes)
+
+
+        headerEditText = findViewById(R.id.header_cr_n)
+        dateEditText = findViewById(R.id.date_cr_n)
+        messageEditText = findViewById(R.id.message_cr_n)
+        createButton = findViewById(R.id.create)
+
+        createButton.setOnClickListener {
+
+            val header = headerEditText.text.toString()
+            val date = dateEditText.text.toString()
+            val message = messageEditText.text.toString()
+
+
+            val intent = Intent().apply {
+                putExtra("header", header)
+                putExtra("date", date)
+                putExtra("message", message)
+            }
+
+
+            setResult(RESULT_OK, intent)
+            finish()
+        }
+    }
+}
