df7fa4175a7780008d19efd69a85d3a02da519fc
ALeksey Strunets <a.strunetss@gmail.com>
2025-01-04 23:38:34 +0300

    ls26

---
+++ app/src/main/java/com/example/singup_app/presentation/ui/activity/Rest.kt
@@ -0,0 +1,67 @@
+package com.example.singup_app.presentation.ui.activity
+
+import android.os.Bundle
+import android.os.Parcelable
+import android.util.Log
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import com.bumptech.glide.Glide
+import com.example.singup_app.R
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+class Rest : AppCompatActivity() {
+
+    val retrofit = Retrofit.Builder().baseUrl("https://collectionapi.metmuseum.org").addConverterFactory(GsonConverterFactory.create()).build()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rest2)
+        val buttonClick =   findViewById<AppCompatButton>(R.id.buttonClick)
+        val image = findViewById<AppCompatImageView>(R.id.image)
+        val api = retrofit.create(Server::class.java)
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            val currentBody = api.getSomeObject(45)
+
+            Glide.with(this@Rest).load(currentBody.objectId).into(image)
+        }
+        buttonClick.setOnClickListener{
+            lifecycleScope.launch(Dispatchers.IO) {
+                val responseBody = api.getAllObjects()
+                Log.d("Response","$responseBody")
+            }
+        }
+    }
+}
+ interface Server {
+     @GET("/public/collection/v1/objects")
+     suspend fun getAllObjects(): Objects
+
+     @GET("/public/collection/v1/objects/{objectId}")
+     suspend fun getSomeObject(@Path(value = "objectId") objectId: Int): SomeObject
+ }
+
+@kotlinx.parcelize.Parcelize
+data class Objects(
+    @SerializedName("total") val total: Int,
+    @SerializedName("objectIds") val objectIds:ArrayList<Int>
+): Parcelable
+
+@kotlinx.parcelize.Parcelize
+data class SomeObject(
+    @SerializedName("objectID") val objectId : Int,
+    @SerializedName("primaryImage") val primaryImage : String
+):Parcelable
